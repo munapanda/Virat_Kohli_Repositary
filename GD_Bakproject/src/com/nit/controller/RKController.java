9b63e17244b545acbac54534fbde3902d834d428
dev1 <dev@gmail.com>
2020-09-30 11:03:55 +0530

    commited

--- GD_Bakproject/src/com/nit/controller/RKController.java
+++ GD_Bakproject/src/com/nit/controller/RKController.java
@@ -3,6 +3,7 @@ package com.nit.controller;
 public class RKController {
 	public static void main(String[] args) {
 		System.out.println("Jitu controller passed");
+		System.out.println("gooing good");
 	}
 
 }
